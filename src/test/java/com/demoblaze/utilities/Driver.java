a25918e9abacf6ecc186dfc624891a7dcf9fedf2
boyacioglu <hansbuda16@gmail.com>
2022-02-20 22:05:48 +0100

    For jenkins

--- src/test/java/com/demoblaze/utilities/Driver.java
+++ src/test/java/com/demoblaze/utilities/Driver.java
@@ -72,7 +72,8 @@ public class Driver {
                     ChromeOptions chromeOptions = new ChromeOptions();
                     chromeOptions.setCapability("platform", Platform.ANY);
                     try {
-                        driverPool.set(new RemoteWebDriver(new URL("http://35.170.246.36:4444/wd/hub"),chromeOptions));
+                        driverPool.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),chromeOptions));
+                        // driverPool.set(new RemoteWebDriver(new URL("http://35.170.246.36:4444/wd/hub"),chromeOptions));
                     } catch (MalformedURLException e) {
                         e.printStackTrace();
                     }
